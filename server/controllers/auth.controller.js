fcc2c217c9cd4754508a09d99dcba610861ea8a9
pranulkbv28 <28bvpranaav@gmail.com>
2024-05-16 00:35:42 +0530

    completed auth routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/!modelExports.js";
 import bcryptjs from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signUpUser = async (req, res) => {
   try {
@@ -32,14 +33,21 @@ export const signUpUser = async (req, res) => {
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      userName: newUser.userName,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({
+        error: "Invalid User Data!!",
+      });
+    }
   } catch (error) {
     console.log("ERROR IN SIGNUP!: ", error.message);
     res.status(500).json({
@@ -50,14 +58,47 @@ export const signUpUser = async (req, res) => {
 
 export const logInUser = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    // You need to add your login logic here
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcryptjs.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({
+        error: "Invalid username or password",
+      });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      userName: user.userName,
+      profilePic: user.profilePic,
+    });
   } catch (error) {
-    console.log("ERROR IN LOGIN!: ", error);
+    console.log("ERROR IN LOGIN!: ", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
   }
 };
 
 export const logOutUser = (req, res) => {
-  console.log("We will be waiting for you! 🤧");
-  // You need to add your logout logic here
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+    });
+    res.status(200).json({
+      message: "Logged Out Successfull!!",
+    });
+  } catch (error) {
+    console.log("ERROR IN LOGOUT!: ", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
