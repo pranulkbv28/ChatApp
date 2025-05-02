ab55ab01430110c6bda86417f7cc58290f4167a2
pranulkbv28 <28bvpranaav@gmail.com>
2025-05-02 13:43:09 +0530

    refactor: update parameter name in catch-all route handler for clarity

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use("/api/users", userRoute);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
